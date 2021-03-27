15878279ad323b1a215ee5720bdc21e6e4b9f3a9
@kozlov_nikita <k.nikitaigorevich@gmail.ru>
2021-03-27 10:03:32 +0600

    Upload file

--- src/main/java/main/config/MvcConfig.java
+++ src/main/java/main/config/MvcConfig.java
@@ -14,6 +14,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file:///"+uploadPath);
+                .addResourceLocations("file://"+uploadPath);
     }
 }
